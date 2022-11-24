b041c42ec67fedd1b8785630ea3a877a335a2629
falamarcao <falamarcao@gmail.com>
2022-11-24 20:03:36 -0300

    134. Adding a "Meals" Component

---
+++ src/components/Meals/AvailableMeals/index.jsx
@@ -0,0 +1,14 @@
+import DUMMY_MEALS from "./dummyMeals";
+import styles from "./AvailableMeals.module.css";
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+
+  return (
+    <section className={styles.meals}>
+      <ul>{mealsList}</ul>
+    </section>
+  );
+};
+
+export default AvailableMeals;
