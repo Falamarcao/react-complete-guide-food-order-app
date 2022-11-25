207da057c693b25cf3b44f0d02ede43dc34fca63
falamarcao <falamarcao@gmail.com>
2022-11-25 16:07:44 -0300

    141. Adding a Cart Context

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
+import { useState } from "react";
+
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals";
 import Cart from "./components/Cart/Cart";
-import { useState } from "react";
+import CartProvider from "./store/CartProvider";
 
 function App() {
   const [IsCartVisibile, setIsCartVisibile] = useState(false);
   const handleClick = () => setIsCartVisibile((prevState) => !prevState);
 
   return (
-    <>
+    <CartProvider>
       {IsCartVisibile ? <Cart onClose={handleClick} /> : null}
       <Header onClickCart={handleClick} />
       <main>
         <Meals />
       </main>
-    </>
+    </CartProvider>
   );
 }
 
