9b3fcecd4ff7884d240dc275935ee649961d9fb3
falamarcao <falamarcao@gmail.com>
2022-11-30 18:57:24 -0300

    148. Using the useEffect Hook

--- src/components/Layout/HeaderCartButton/index.jsx
+++ src/components/Layout/HeaderCartButton/index.jsx
@@ -1,9 +1,11 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartContext from "../../../store/cart-context";
 import CartIcon from "../../Icons/CartIcon";
 import styles from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const [isAnimated, setIsAnimated] = useState(false);
+
   const cartContext = useContext(CartContext);
 
   const itemsCount = cartContext.items.reduce(
@@ -11,8 +13,18 @@ const HeaderCartButton = (props) => {
     0
   );
 
+  const buttonStyles = [styles.button, isAnimated && styles.bump].join(" ");
+
+  useEffect(() => {
+    if (cartContext.items.length > 0) {
+      setIsAnimated(true);
+      const timeoutId = setTimeout(() => setIsAnimated(false), 300);
+      return () => clearTimeout(timeoutId); // best pratice to clearTimeout
+    }
+  }, [cartContext.items]);
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={buttonStyles} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
